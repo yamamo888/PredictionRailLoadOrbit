ec48e812317b52667dcd076367e828f6ee5a8a79
kuroshum <kurora.shumpei@g.wakayama-u.jp>
2018-12-11 18:17:57 +0900

    kuroshum

--- code/RailLoadOrbit.py
+++ code/RailLoadOrbit.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
 
     mytrackData = trackData(trackfiles)
     mytrackData.NaN()
-    #pdb.set_trace()
+    pdb.set_trace()
     xData = mytrackData.A.drop('hlr',axis=1)
     tData = mytrackData.A[['date','hlr']]
     
