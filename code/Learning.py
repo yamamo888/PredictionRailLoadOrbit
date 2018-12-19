92f301a9b52a4b229398114a1979896b1aea880d
kuroshum <kurora.shumpei@g.wakayama-u.jp>
2018-12-19 11:11:02 +0900

    ARMAモデル実装(多分)

--- code/Learning.py
+++ code/Learning.py
@@ -48,14 +48,31 @@ class Arima():#自己回帰
         self.w_ar = np.random.normal(0.0, pow(100, -0.5), (self.p + 1, 1))
         self.w_ma = np.random.normal(0.0, pow(100, -0.5), (self.q + 1, 1))
 
-        self.eps = np.array([np.random.normal(1,25) for _ in range((self.q-self.d)*self.t.shape[0])])
+        self.eps = np.array([np.random.normal(1,25) for _ in range(365*self.t.shape[0])])
         print(self.eps)
 
     def train(self):
         date_ar = []
-        #date_ma = []
-        z_ar1 = np.empty((0, (self.p-self.d)*self.t.shape[0]))
-        z_ma1 = np.empty((0, (self.q-self.d)*self.t.shape[0]))
+        date_ma = []
+        z_ar1 = np.empty(((self.N-self.d)*self.t.shape[0],0))
+        z_ma1 = np.empty(((self.N-self.d)*self.t.shape[0],0))
+        for i in range(self.p):
+            z_ar0 = []
+            for j in range(self.N-self.d):
+                date_ar = np.append(date_ar, (self.t['date'][-1:] - datetime.timedelta(days=j+i+2)).astype(str))
+                z_ar0 = np.append(z_ar0, self.tData[self.tData['date'] == date_ar[-1]]['hll'])
+            z_ar0 = z_ar0[np.newaxis].T
+            z_ar1 = np.append(z_ar1, z_ar0,axis=1)
+
+        for i in range(self.q):
+            z_ma0 = []
+            for j in range(self.N-self.d):
+                z_ma0 = np.append(z_ma0, self.eps[(j+i+2)*self.t.shape[0]:(j+i+3)*self.t.shape[0]]) 
+                #date_ma = np.append(date_ma, (self.t['date'][-1:] - datetime.timedelta(days=j+i+2)).astype(str))
+                #z_ma0 = np.append(z_ma0, self.tData[self.tData['date'] == date_ar[-1]]['hll'])
+            z_ma0 = z_ma0[np.newaxis].T
+            z_ma1 = np.append(z_ma1, z_ma0,axis=1)
+        """
         for i in range(self.N):
             z_ar0 = []
             z_ma0 = []
@@ -66,32 +83,37 @@ class Arima():#自己回帰
             z_ar1 = np.append(z_ar1, z_ar0,axis=0)
 
             for k in range(self.q-self.d):
-                z_ma0 = np.append(z_ma0, self.eps[k*self.t.shape[0]:(k+1)*self.t.shape[0]]) 
+                z_ma0 = np.append(z_ma0, self.eps[(k+i+2)*self.t.shape[0]:(k+i+3)*self.t.shape[0]]) 
+                #pdb.set_trace()
                 #date_ma = np.append(date_ma, (self.t['date'][-1:] - datetime.timedelta(days=k+i+2)).astype(str))
                 #z_ma0 = np.append(z_ma0, self.tData[self.tData['date'] == date_ma[-1]]['hll'])
+            #pdb.set_trace()
             z_ma0 = z_ma0[np.newaxis]
             z_ma1 = np.append(z_ma1, z_ma0,axis=0)
-            
-        z_ar1 = np.append(z_ar1, np.ones([1,z_ar1.shape[1]]),axis=0)
-        z_ma1 = np.append(z_ma1, np.ones([1,z_ma1.shape[1]]),axis=0)
+        """    
+        z_ar1 = np.append(z_ar1, np.ones([z_ar1.shape[0],1]),axis=1)
+        z_ma1 = np.append(z_ma1, np.ones([z_ma1.shape[0],1]),axis=1)
 
         y = []
         date_y = []
+        e = []
         for i in range(self.N-self.d):
             date_y = np.append(date_y, (self.t['date'][-1:] - datetime.timedelta(days=i+1)).astype(str))
             y = np.append(y, self.tData[self.tData['date'] == date_y[-1]]['hll'])
+            e = np.append(e, self.eps[i*self.t.shape[0]:(i+1)*self.t.shape[0]])
         y = y[np.newaxis].T
+        e = e[np.newaxis].T
 
-        sigma_ar0 = np.matmul(z_ar1, z_ar1.T)
-        sigma_ar1 = np.matmul(z_ar1, y)
+        sigma_ar0 = np.matmul(z_ar1.T, z_ar1)
+        sigma_ar1 = np.matmul(z_ar1.T, y)
         self.w_ar = np.matmul(sigma_ar0, sigma_ar1)
         print(self.w_ar)
+        pdb.set_trace()
 
-        sigma_ma0 = np.matmul(z_ma1, z_ma1.T)
-        sigma_ma1 = np.matmul(z_ma1, self.eps[np.newaxis].T)
+        sigma_ma0 = np.matmul(z_ma1.T, z_ma1)
+        sigma_ma1 = np.matmul(z_ma1.T, e)
         self.w_ma = np.matmul(sigma_ma0, sigma_ma1)
         print(self.w_ma)
-        #pdb.set_trace()
 
     def predict(self,t):
         #pdb.set_trace()
